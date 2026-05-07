cc8ec9da3051bf0e5a80c758adcf1de9cb565192
Michael Kalaba <xkalaba@fi.muni.cz>
2026-05-07 08:39:18 +0200

    fix: fix assert for password auth, replace load with safe_load

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -37,12 +37,14 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
 def authenticate(password):
     # Assert that the password is correct
-    assert password == "Iloveyou", "Invalid password!"
+    if password != "Iloveyou":
+        print("Invalid password!")
+        return
     print("Successfully authenticated!")
 
 if __name__ == '__main__':
