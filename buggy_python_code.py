03fb1c44201cdcd95e8e9e2ed5cc133526184b89
Michael Kalaba <xkalaba@fi.muni.cz>
2026-05-07 08:51:13 +0200

    fix: fix injection

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -25,9 +25,13 @@ def print_nametag(format_string, person):
 
 def fetch_website(urllib_version, url):
     # Import the requested version (2 or 3) of urllib
-    exec(f"import urllib{urllib_version} as urllib", globals())
+    if urllib_version == 2:
+        exec(f"import urllib{2} as urllib", globals())
+    elif urllib_version == 3:
+        exec(f"import urllib{3} as urllib", globals())
+    else:
+        return
     # Fetch and print the requested URL
- 
     try: 
         http = urllib.PoolManager()
         r = http.request('GET', url)
